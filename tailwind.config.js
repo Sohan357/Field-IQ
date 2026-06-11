08f25c94f732aaa4aece9b73ac62396d3d68d688
SohanKothakapa <sohankothakapa38@gmail.com>
2026-06-11 19:15:53 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        forest: {
+          50: '#f0fdf4',
+          100: '#dcfce7',
+          200: '#bbf7d0',
+          300: '#86efac',
+          400: '#4ade80',
+          500: '#22c55e',
+          600: '#16a34a',
+          700: '#15803d',
+          800: '#166534',
+          900: '#14532d',
+        },
+        earth: {
+          50: '#faf8f5',
+          100: '#f3ede4',
+          200: '#e6d9c8',
+          300: '#d4bf9e',
+          400: '#b89a6f',
+          500: '#9a7b52',
+        },
+      },
+      fontFamily: {
+        sans: ['"DM Sans"', 'system-ui', 'sans-serif'],
+        display: ['"Fraunces"', 'Georgia', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+};
